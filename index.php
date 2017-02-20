6bcf870462d8d00dbdb80f15c4698ecd382a4477
Raju Pangeni <evit.programmer@gmail.com>
2017-02-20 10:43:10 +0545

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>learn js</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+<header>
+	<h1>Quiz Ninja!</h1>
+</header>
+	<script src="js/script.js"></script>
+</body>
+</html>
