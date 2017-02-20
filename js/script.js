b6eac13ac15f3f8fc12f717e971b01284614decf
Raju Pangeni <evit.programmer@gmail.com>
2017-02-20 14:16:16 +0545

    all functions changed into objects

--- js/script.js
+++ js/script.js
@@ -1,32 +1,30 @@
-	alert("Welcome to Quiz Ninja!");
-
-	var quiz = [
-		["What is Superman's real name?","Clarke Kent"],
-		["What is Wonderwoman's real name?","Dianna Prince"],
-		["What is Batman's real name?","Bruce Wayne"]
-	];
+	alert("hello");
+	quiz = {
+		"name":"Super Hero Name Quiz",
+		"description":"How many super heroes can you name?",
+		"question":"What is the real name of ",
+		"questions": [
+			{ "question": "Superman", "answer": "Clarke Kent" },
+			{ "question": "Batman", "answer": "Bruce Wayne" },
+			{ "question": "Wonder Woman", "answer": "Dianna Prince" }
+		]
+	}
 
 	var score = 0;
 
-	play(quiz);
-
-	function play(quiz){
-		// main game loop
-		for(var i=0, question, answer, max=quiz.length; i<max; i++) {
-			question = quiz[i][0];
-			answer = ask(question);
-			check(answer);
-		}
-		// end of main game loop
-		gameOver();	
+	for(var i=0, question, answer, max=quiz.questions.length; i<max;i++) {
+		question = quiz.questions[i].question; // change is made here
+		answer = ask(question);
+		check(answer);
 	}
-
+	gameOver();
+	
 	function ask(question) {
-		return prompt(question); // quiz[i][0] is the ith questions
+		return prompt(quiz.question+question); // quiz[i][0] is the ith questions
 	}
 
 	function check(answer) {
-		if(answer === quiz[i][1]){ // quiz[i][1] is the ith answer	
+		if(answer === quiz.question[i].answer){ // quiz[i][1] is the ith answer	
 			alert("Correct!");
 			// increase score by 1
 			score++;
