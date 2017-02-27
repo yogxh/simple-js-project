450e6aa59eeb116af6084e91724dcd91e78afaab
yogxh <yogxacharya@gmail.com>
2017-02-27 23:46:39 +0545

    Update

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 
+
 /* identify different element and store value in variable */
-var $question = document.getElementById("question");
+var html_question = document.getElementById("question");
 var $feedback = document.getElementById("feedback");
 var $score = document.getElementById("score");
 var $start = document.getElementById("start");
-
 var score = 0;
 
 update($score,score);
 
 /* The following function helps to update the question,answer,and score as well */
 function update(element,content,klass){
-	var p = element.firstChild || document.createElement("p");
+	var p = element.firsChild || document.createElement("p");
 	p.textContent = content;
 	element.appendChild(p);
 	if(klass){
@@ -37,27 +37,26 @@ $start.addEventListener('click',function(){ play(quiz) },false);
 
 /* Updates the initial score and hroughout the game play */
 function play(quiz){
-	ask();
-	check();
+	for(var i=0, question, answer; i<quiz.questions.length;i++) {
+		question = quiz.questions[i].question; // change is made here
+		update(html_question,quiz.question+question);
+		ask(question,i);
+	}
 	gameOver();
 }
 
 
-for(var i=0, question, answer, max=quiz.questions.length; i<max;i++) {
 
-	question = quiz.questions[i].question; // change is made here
-	ask(question);
-	check(answer);
-}
 
 /* function to ask question */
-function ask(question) {
-	update($question,quiz.question+question);
+function ask(question,i) {
+
 	answer=prompt("Enter your answer");
+	check(answer,i);
 }
 
 /* function to check answer and update score */
-function check(answer) {
+function check(answer,i) {
 		if(answer === quiz.questions[i].answer){ // quiz[i][1] is the ith answer	
 		update($feedback,"Correct!","right");
 		// increase score by 1
